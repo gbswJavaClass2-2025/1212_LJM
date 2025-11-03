c2b846eee262d25aca1b054b961b495dcae89486
kingjangjo <araloveasalll@gmail.com>
2025-11-03 22:24:09 +0900

    asdf

--- cote/src/exam05/BookExample.java
+++ cote/src/exam05/BookExample.java
@@ -3,10 +3,10 @@ package exam05;
 public class BookExample {
 
 	public static void main(String[] args) {
-		Book[] books = null;
-		books[1] = new Book("Your Name","shin","11111111",9000);
+		Book[] books = new Book[3];
+		books[0] = new Book("Your Name","shin","11111111",9000);
 		books[1] = new Book("AnlSi","shin","12111111",9000);
-		books[1] = new Book("suzume","shin","13111111",9000);
+		books[2] = new Book("suzume","shin","13111111",9000);
 	}
 
 }
