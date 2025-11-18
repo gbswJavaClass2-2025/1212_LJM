4dc6ebea1bd1fd97b5f32b5739da31982bb9e8b4
kingjangjo <araloveasalll@gmail.com>
2025-11-18 11:19:44 +0900

    11/18 과제 끝/[10과]

--- ex04/src/exam04/HospitalMain.java
+++ ex04/src/exam04/HospitalMain.java
@@ -4,7 +4,7 @@ public class HospitalMain {
 
 	public static void main(String[] args) {
 		Doctor doctor = new Doctor("Lee");
-		Person person = doctor;
+		Person person = doctor;	
 		person.work();
 	}
 
